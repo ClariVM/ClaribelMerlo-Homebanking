f6e4faf8772a18ddb0a9b8c7327a9b484ea31f6e
ClariVM <claribelmerlo@hotmail.com>
2023-08-22 10:25:42 -0300

    Implement client registration and login

--- src/main/java/com/mindhub/homebanking/models/Client.java
+++ src/main/java/com/mindhub/homebanking/models/Client.java
@@ -17,6 +17,7 @@ public class Client {
     private String firstName;
     private String lastName;
     private String email;
+    private String password;
 
     @OneToMany(mappedBy = "owner", fetch = FetchType.EAGER)
     private Set<Account> accounts = new HashSet<>();
@@ -30,10 +31,11 @@ public class Client {
     public Client() {
     }
 
-    public Client(String firstName, String lastName, String email) {
+    public Client(String firstName, String lastName, String email, String password) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.password = password;
     }
 
     public List<Loan> getLoans(){
@@ -70,6 +72,14 @@ public class Client {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Set<Account> getAccounts() {
